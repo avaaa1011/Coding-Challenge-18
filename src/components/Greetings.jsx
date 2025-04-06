f13bbe39a23caa93d69043f5d22f234cdc423fc2
Ava Tran <ava.tran1011@gmail.com>
2025-04-06 17:29:15 -0400

    task 1: creating a new component

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,13 @@
+//task 1: creating a new component 
+import React from "react";
+ 
+ function Greetings() {
+     return (
+         <div>
+             <h2>Hello from the Greeting Component!</h2>
+             <p>This is your first custom component in React.</p>
+         </div>
+     );
+ }
+ 
+ export default Greetings;
